70795416f50ef8bc6fc1d8e08e1974a1bd8b8fdc
Finnbyte <eelpy@protonmail.com>
2024-01-30 23:23:26 +0200

    Optimize by limiting times when drawing methods are ran
    For example, panel only needs to change when editing mode changes.
    It is extremely inefficient to render it every frame again.

--- main.py
+++ main.py
@@ -19,37 +19,37 @@ def main():
     SCREEN = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
     font = pygame.font.SysFont(None, 32)
 
-    cell_grid = CellGrid(GRID_SIZE, TOP_OFFSET)
+    grid_surface = pygame.Surface((GRID_SIZE*CELL_SIZE, GRID_SIZE*CELL_SIZE))
+
+    cell_grid = CellGrid(grid_surface, GRID_SIZE, TOP_OFFSET)
     cell_grid.initialize_cells()
 
     top_panel_surface = pygame.Surface((WINDOW_WIDTH, TOP_OFFSET))
-    top_panel = TopPanel(font)
+    top_panel = TopPanel(top_panel_surface, font)
 
-    grid_surface = pygame.Surface((GRID_SIZE, GRID_SIZE))
+    cell_grid.draw(grid_surface)
+    top_panel.render(is_editing_grid=True)
 
     running = True
     while running:
         for event in pygame.event.get():
             if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
                 cell_grid.is_editing_grid = not cell_grid.is_editing_grid
+                top_panel.render(cell_grid.is_editing_grid)
             if event.type == pygame.MOUSEBUTTONUP:
                 cell_grid.handle_click_on_cell(pygame.mouse.get_pos())
             if event.type == pygame.QUIT:
                 running = False
-                pygame.quit()
 
         SCREEN.blit(top_panel_surface, (0, 0))
-        top_panel.draw(top_panel_surface, cell_grid.is_editing_grid)
-
         SCREEN.blit(grid_surface, (0, TOP_OFFSET))
-        cell_grid.draw(grid_surface)
 
         if not cell_grid.is_editing_grid:
             cell_grid.update()
 
         pygame.display.update()
-        
-    sys.exit()
+
+    pygame.quit()
 
 if __name__ == "__main__":
     import cProfile as profile
--- top_panel.py
+++ top_panel.py
@@ -1,9 +1,10 @@
 from colors import BLACK, WHITE
 
 class TopPanel:
-    def __init__(self, font):
+    def __init__(self, surface, font):
         self.font = font
+        self.surface = surface
         
-    def draw(self, surface, is_editing_grid):
-        surface.fill(WHITE)
-        surface.blit(self.font.render(f"{'Editing' if is_editing_grid else 'Simulating'}", True, BLACK), (0,0))
+    def render(self, is_editing_grid):
+        self.surface.fill(WHITE)
+        self.surface.blit(self.font.render(f"{'Editing' if is_editing_grid else 'Simulating'}", True, BLACK), (0,0))
