b2a095fb9cf7e3d390571cb86bbe0fd6e61fde68
Finnbyte <eelpy@protonmail.com>
2024-04-07 16:35:39 +0300

    Remove _prepare method

--- game/game.py
+++ game/game.py
@@ -34,20 +34,14 @@ class Game:
         self.top_panel = TopPanel(pygame.Surface((WINDOW_WIDTH, TOP_PANEL_HEIGHT)), font)
 
     def run_until_quit(self):
-        self._prepare()
         clock = pygame.time.Clock()
+        self.top_panel.draw(self.state)
+        GridRenderer.prepare_grid(self.grid_surface)
         while True:
             clock.tick(FPS)
             self._handle_events(pygame.event.get())
             self._update()
 
-    def _prepare(self):
-        self.top_panel.draw(self.state)
-        for x in range(WINDOW_WIDTH // CELL_SIZE):
-            for y in range(WINDOW_HEIGHT // CELL_SIZE):
-                pygame.draw.rect(self.grid_surface, WHITE, pygame.Rect(x*CELL_SIZE, y*CELL_SIZE,
-                    CELL_SIZE, CELL_SIZE), 1)
-        
     def _handle_events(self, events: list[pygame.event.Event]):
         for event in events:
             if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
